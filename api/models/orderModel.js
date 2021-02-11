5792a090e22f669195bdc33d556d0b060f497fd2
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-11 21:05:24 +0700

    feat: Add database model for order

---
+++ api/models/orderModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const validate = require('validator');
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'An order must have a user!'],
+  },
+  room: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+    required: [true, 'An order must have a room!'],
+  },
+  employee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Employee',
+    required: [true, 'An order must have an employee!'],
+  },
+  startDate: {
+    type: Date,
+    required: [true, 'An order must have a start date!'],
+    validate: [
+      validate.isAfter,
+      'An order must be booked starting from today!',
+    ],
+  },
+  endDate: {
+    type: Date,
+    required: [true, 'An order must have an end date!'],
+  },
+  status: {
+    type: String,
+    enum: ['ordered', 'processed', 'accepted', 'finished', 'cancelled'],
+    required: [true, 'An order must have a status!'],
+  },
+  totalPrice: {
+    type: Number,
+    required: [true, 'An order must have a total price!'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Order = mongoose.Model('Order', orderSchema);
+
+module.exports = Order;
