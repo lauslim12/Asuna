38e69a30781dd2b86a9981b5186e809eef249cd1
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-18 17:15:39 +0700

    feat: Add interactivity with state

--- web/components/ListOfRooms/index.jsx
+++ web/components/ListOfRooms/index.jsx
@@ -1,18 +1,49 @@
-import { Grid, Heading, HStack, Spacer, VStack } from '@chakra-ui/react';
+/**
+ * Current maximum floor in this building is 2. Change in production. (Environment variable)
+ */
+
+import { Grid, Heading, HStack, IconButton, Spacer, VStack } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { AiFillMinusCircle, AiFillPlusCircle } from 'react-icons/ai';
 
 import RoomCard from './RoomCard';
 
 const ListOfRooms = ({ rooms }) => {
+  const [roomsInFloor, setRoomsInFloor] = useState([]);
+  const [currentFloor, setCurrentFloor] = useState(1);
+
+  useEffect(() => {
+    const roomsInCurrentFloor = rooms.filter((room) => room.floor.number === currentFloor);
+
+    setRoomsInFloor(roomsInCurrentFloor);
+  }, [rooms, currentFloor]);
+
   return (
     <VStack align="stretch" spacing={4}>
       <HStack>
         <Heading size="md">Rooms</Heading>
         <Spacer />
-        <Heading size="md">Floor 1</Heading>
+        <HStack spacing={2}>
+          <Heading size="md">{`Floor ${currentFloor}`}</Heading>
+          <IconButton
+            aria-label="Previous floor"
+            icon={<AiFillMinusCircle />}
+            variant="outline"
+            size="xs"
+            onClick={() => setCurrentFloor((previousFloor) => Math.max(previousFloor - 1, 1))}
+          />
+          <IconButton
+            aria-label="Next floor"
+            icon={<AiFillPlusCircle />}
+            variant="outline"
+            size="xs"
+            onClick={() => setCurrentFloor((previousFloor) => Math.min(previousFloor + 1, 2))}
+          />
+        </HStack>
       </HStack>
       <Grid templateColumns="repeat(auto-fill, minmax(9rem, 1fr))" gap={2}>
-        {rooms.map((room) => (
+        {roomsInFloor.map((room) => (
           <RoomCard key={room.slug} roomData={room} />
         ))}
       </Grid>
