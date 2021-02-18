8fa464d59b5a658652e7be32f000d6fc419fac2e
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-18 18:19:09 +0700

    feat: Implement NProgress

---
+++ web/components/NProgress.jsx
@@ -0,0 +1,35 @@
+/* This is adapted from https://github.com/chiyadev/genshin-schedule. */
+import Router from 'next/router';
+import { done, start } from 'nprogress';
+import { useEffect } from 'react';
+
+const NProgress = () => {
+  useEffect(() => {
+    // timeout prevents shallow routing from causing the progress bar to show
+    let startTimeout;
+
+    const handleStart = () => {
+      clearTimeout(startTimeout);
+      startTimeout = window.setTimeout(start);
+    };
+
+    const handleEnd = () => {
+      clearTimeout(startTimeout);
+      done();
+    };
+
+    Router.events.on('routeChangeStart', handleStart);
+    Router.events.on('routeChangeComplete', handleEnd);
+    Router.events.on('routeChangeError', handleEnd);
+
+    return () => {
+      Router.events.off('routeChangeStart', handleStart);
+      Router.events.off('routeChangeComplete', handleEnd);
+      Router.events.off('routeChangeError', handleEnd);
+    };
+  }, []);
+
+  return null;
+};
+
+export default NProgress;
--- web/pages/_app.jsx
+++ web/pages/_app.jsx
@@ -1,10 +1,18 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
+import '../components/NProgress.css';
+
 import { ChakraProvider } from '@chakra-ui/react';
 
+import NProgress from '../components/NProgress';
+import scrollPreserver from '../utils/scrollPreserver';
+
 const App = ({ Component, pageProps }) => {
+  scrollPreserver();
+
   return (
     <ChakraProvider>
+      <NProgress />
       <Component {...pageProps} />
     </ChakraProvider>
   );
