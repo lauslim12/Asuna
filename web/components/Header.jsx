a45ff1f1ad61872ccb76d1085f6455c890c087a6
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-18 15:39:01 +0700

    feat: Update header, remove background

--- web/components/Header.jsx
+++ web/components/Header.jsx
@@ -1,17 +1,26 @@
-import { Box, HStack, Spacer } from '@chakra-ui/react';
+import { chakra, HStack, Icon, Image, Spacer } from '@chakra-ui/react';
+import { HiOfficeBuilding, HiUser } from 'react-icons/hi';
 
 const Header = () => {
   return (
-    <HStack p={3} spacing={2} bg="gray.700" color="white">
-      <Box>
-        <p>Logo.</p>
-      </Box>
+    <HStack as="nav" p={4} spacing={2} bg="white" color="black">
+      <HStack spacing={2}>
+        <Image src="/logo.png" w={6} h={6} borderRadius="md" />
+        <chakra.span fontSize="lg">Project Asuna</chakra.span>
+      </HStack>
 
       <Spacer />
 
-      <Box>
-        <p>Profile.</p>
-      </Box>
+      <HStack spacing={4}>
+        <HStack spacing={2}>
+          <Icon as={HiOfficeBuilding} />
+          <chakra.div>Rooms</chakra.div>
+        </HStack>
+        <HStack spacing={2}>
+          <Icon as={HiUser} />
+          <chakra.div>Profile</chakra.div>
+        </HStack>
+      </HStack>
     </HStack>
   );
 };
