b573627d79ea473ea3ddc63b94c15914ebcd74d7
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-18 17:16:32 +0700

    feat: Add icons and links

--- web/components/Header.jsx
+++ web/components/Header.jsx
@@ -1,25 +1,39 @@
-import { chakra, HStack, Icon, Image, Spacer } from '@chakra-ui/react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { chakra, HStack, Icon, Image, Link, Spacer } from '@chakra-ui/react';
+import NextLink from 'next/link';
 import { HiOfficeBuilding, HiUser } from 'react-icons/hi';
 
 const Header = () => {
   return (
     <HStack as="nav" p={4} spacing={2} bg="white" color="black">
-      <HStack spacing={2}>
-        <Image src="/logo.png" w={6} h={6} borderRadius="md" />
-        <chakra.span fontSize="lg">Project Asuna</chakra.span>
-      </HStack>
+      <NextLink href="/" passHref>
+        <Link flexShrink={0}>
+          <HStack spacing={2}>
+            <Image src="/logo.png" w={6} h={6} borderRadius="md" />
+            <chakra.span fontSize="lg">Project Asuna</chakra.span>
+          </HStack>
+        </Link>
+      </NextLink>
 
       <Spacer />
 
       <HStack spacing={4}>
-        <HStack spacing={2}>
-          <Icon as={HiOfficeBuilding} />
-          <chakra.div>Rooms</chakra.div>
-        </HStack>
-        <HStack spacing={2}>
-          <Icon as={HiUser} />
-          <chakra.div>Profile</chakra.div>
-        </HStack>
+        <NextLink href="/rooms" passHref>
+          <Link flexShrink={0}>
+            <HStack spacing={2}>
+              <Icon as={HiOfficeBuilding} />
+              <chakra.div>Rooms</chakra.div>
+            </HStack>
+          </Link>
+        </NextLink>
+        <NextLink href="/profile" passHref>
+          <Link flexShrink={0}>
+            <HStack spacing={2}>
+              <Icon as={HiUser} />
+              <chakra.div>Profile</chakra.div>
+            </HStack>
+          </Link>
+        </NextLink>
       </HStack>
     </HStack>
   );
