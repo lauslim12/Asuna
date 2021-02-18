3248fc92abde0ce61c7005b74c2f834565e233d1
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-18 15:38:36 +0700

    feat: Add top bar for UX

--- web/components/ListOfRooms/index.jsx
+++ web/components/ListOfRooms/index.jsx
@@ -1,4 +1,4 @@
-import { Grid, Heading, VStack } from '@chakra-ui/react';
+import { Grid, Heading, HStack, Spacer, VStack } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 
 import RoomCard from './RoomCard';
@@ -6,7 +6,11 @@ import RoomCard from './RoomCard';
 const ListOfRooms = ({ rooms }) => {
   return (
     <VStack align="stretch" spacing={4}>
-      <Heading size="md">Rooms</Heading>
+      <HStack>
+        <Heading size="md">Rooms</Heading>
+        <Spacer />
+        <Heading size="md">Floor 1</Heading>
+      </HStack>
       <Grid templateColumns="repeat(auto-fill, minmax(9rem, 1fr))" gap={2}>
         {rooms.map((room) => (
           <RoomCard key={room.slug} roomData={room} />
