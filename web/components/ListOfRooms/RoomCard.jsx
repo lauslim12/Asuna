60349f27c11874e0d8d34bcc5a1837311b421adb
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-18 17:16:09 +0700

    feat: Add currency separator and link

--- web/components/ListOfRooms/RoomCard.jsx
+++ web/components/ListOfRooms/RoomCard.jsx
@@ -1,29 +1,42 @@
 import { Center, Image, Text, VStack } from '@chakra-ui/react';
+import NextLink from 'next/link';
 import PropTypes from 'prop-types';
 
 const RoomCard = ({ roomData }) => {
   return (
-    <VStack
-      align="stretch"
-      bg="white"
-      borderWidth={1}
-      borderColor="gray.200"
-      borderRadius="md"
-      p={4}
-      spacing={2}
-    >
-      <Center minH={0} flex={1}>
-        <Image
-          src={`${process.env.NEXT_PUBLIC_API_URL}/images/thumbnails/${roomData.thumbnail}`}
-          h="full"
-          borderRadius="full"
-        />
-      </Center>
-      <VStack spacing={2}>
-        <Text>{roomData.name}</Text>
-        <Text fontSize="xs">{`Rp. ${roomData.price}`}</Text>
+    <NextLink href={`/rooms/${roomData.slug}`} passHref>
+      <VStack
+        align="stretch"
+        bg="white"
+        borderWidth={1}
+        borderColor="gray.200"
+        borderRadius="md"
+        p={4}
+        spacing={2}
+        cursor="pointer"
+      >
+        <Text fontSize="xs" textTransform="capitalize" textAlign="center">
+          {roomData.type}
+        </Text>
+
+        <Center minH={0} flex={1}>
+          <Image
+            src={`${process.env.NEXT_PUBLIC_API_URL}/images/thumbnails/${roomData.thumbnail}`}
+            h="full"
+            borderRadius="full"
+          />
+        </Center>
+
+        <VStack spacing={2}>
+          <Text>{roomData.name}</Text>
+          <Text fontSize="xs">
+            {`Rp. ${roomData.price.toLocaleString('id')} / ${
+              roomData.type === 'coworking-space' ? 'hour' : 'month'
+            }`}
+          </Text>
+        </VStack>
       </VStack>
-    </VStack>
+    </NextLink>
   );
 };
 
