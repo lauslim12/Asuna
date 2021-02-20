f0013980859e0a6756eeab4acc06b03580d00fc0
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-20 22:56:50 +0700

    feat: Add base file for homepage

---
+++ web/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react';
+
+import Layout from '../../components/Layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <Box>
+        <p>Hello World!</p>
+      </Box>
+    </Layout>
+  );
+};
+
+export default Home;
