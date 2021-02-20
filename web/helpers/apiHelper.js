e8584c2c4adffb539d3c4028d4618808c4613862
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-20 22:34:43 +0700

    feat: Implement API helper

---
+++ web/helpers/apiHelper.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export const getAuth = async (url, token) => {
+  const response = await axios
+    .get(url, { headers: { Authorization: `Bearer ${token}` } })
+    .catch((err) => err.response);
+
+  return response.data;
+};
+
+export const postAuth = async (data, url, token) => {
+  const response = await axios
+    .post(url, data, { headers: { Authorization: `Bearer ${token}` } })
+    .catch((err) => err.response);
+
+  return response.data;
+};
+
+export const post = async (data, url) => {
+  const response = await axios.post(url, data).catch((err) => err.response);
+
+  return response.data;
+};
+
+export const get = async (url) => {
+  const response = await axios.get(url).catch((err) => err.response);
+
+  return response.data;
+};
