6396214a92bb2db1affb3f1453a92428ba01a611
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-20 23:23:31 +0700

    fix: Add async tag on function

--- web/pages/api/logout.js
+++ web/pages/api/logout.js
@@ -1,11 +1,12 @@
 import Cookies from 'cookies';
 
-export default (req, res) => {
+export default async (req, res) => {
   const cookies = new Cookies(req, res);
 
   cookies.set('jwt', 'loggedOut', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
+    sameSite: 'lax',
   });
 
   return res.status(200).json({
