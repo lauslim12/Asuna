40757740ddbf88258c279150916fbcfddde7278c
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-20 08:29:18 +0700

    feat: Implement API proxy for cross-domain auth

---
+++ web/pages/api/login.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import Cookies from 'cookies';
+
+export default async (req, res) => {
+  const { username, password } = req.body;
+  let response;
+
+  try {
+    response = await axios.post(`${process.env.PRIVATE_API_URL}/api/v1/users/login`, {
+      username,
+      password,
+    });
+  } catch (e) {
+    return res.status(401).json({
+      status: 'fail',
+      response: e.response.data,
+    });
+  }
+
+  const cookies = new Cookies(req, res);
+  cookies.set('jwt', response.data.token, {
+    httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000),
+    sameSite: 'lax',
+  });
+
+  return res.status(200).json({
+    status: 'success',
+  });
+};
