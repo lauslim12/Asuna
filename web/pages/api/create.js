488160c7e06b0ace17d6e6225ae020bac2e5b069
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-25 14:09:28 +0700

    feat: Add internal API for proxy

---
+++ web/pages/api/create.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import Cookies from 'cookies';
+
+export default async (req, res) => {
+  const { entity } = req.body;
+
+  const cookies = new Cookies(req, res);
+  const token = cookies.get('jwt');
+
+  try {
+    await axios.post(`${process.env.PRIVATE_API_URL}/api/v1/${entity}`, req.body, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  } catch (e) {
+    return res.status(401).json({
+      status: 'fail',
+      response: e.response.data,
+    });
+  }
+
+  return res.status(200).json({
+    status: 'success',
+  });
+};
