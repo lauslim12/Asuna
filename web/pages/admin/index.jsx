5ecbefa5894375d67f26091b4854303fb75cc56e
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-25 20:32:04 +0700

    feat: Implement route guard for admin homepage

--- web/pages/admin/index.jsx
+++ web/pages/admin/index.jsx
@@ -1,5 +1,34 @@
 import MenuCard from '../../components/Admin/MenuCard';
 import Layout from '../../components/Layout';
+import { getAuth } from '../../helpers/apiHelper';
+
+export const getServerSideProps = async (ctx) => {
+  const token = ctx.req.cookies.jwt;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanent: false,
+      },
+    };
+  }
+
+  const { data } = await getAuth(`${process.env.PRIVATE_API_URL}/api/v1/users/me`, token);
+
+  if (data.role !== 'admin') {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
 
 const AdminHomepage = () => {
   return (
