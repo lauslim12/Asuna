f2698bf4a4416c0ab68cb7ea5f4b0d4159f9ff55
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-25 09:30:08 +0700

    feat: Add algorithm for order functionality

--- api/controllers/orderController.js
+++ api/controllers/orderController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const asyncHandler = require('../utils/asyncHandler');
 const Order = require('../models/orderModel');
 const User = require('../models/userModel');
@@ -12,6 +13,17 @@ const User = require('../models/userModel');
  */
 exports.changeOrderStatus = asyncHandler(async (req, res, next) => {
   const { status } = req.body;
+  const validStatuses = [
+    'ordered',
+    'processed',
+    'accepted',
+    'finished',
+    'cancelled',
+  ];
+
+  if (!validStatuses.includes(status)) {
+    return next(new AppError('Invalid status!', 400));
+  }
 
   const updatedOrder = await Order.findByIdAndUpdate(
     req.params.id,
@@ -32,5 +44,12 @@ exports.makeAdmin = asyncHandler(async (req, res, next) => {
 });
 
 exports.placeOrder = asyncHandler(async (req, res, next) => {
-  // Pass...
+  // 1. User inputs their request into the system.
+  // 2. Check if there is the same room (in Orders collection) whose end date is higher than the requested start date.
+  // ex: Order.findOne({ $and: [{ room: requestedRoom }, { endDate: { $gte: requestedStartDate } }] })
+  // note: if coworking space, don't invalidate.
+  // 3. If there is, invalidate the request.
+  // 4. If not, calculate the price.
+  // ex: if it is office, then multiply per month, else multiply per hour.
+  // 5. place the order with the 'processed' status.
 });
