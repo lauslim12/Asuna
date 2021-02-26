145adbd29e7033399ccc22508e41e804beae8277
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-26 22:54:13 +0700

    feat: Finish create floor functionality and design

--- web/pages/admin/entities/create/floors.jsx
+++ web/pages/admin/entities/create/floors.jsx
@@ -1,85 +1,144 @@
 import {
   Button,
   ButtonGroup,
+  Flex,
   FormControl,
+  FormHelperText,
+  FormLabel,
   Heading,
+  Icon,
   Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Text,
+  useColorMode,
+  useColorModeValue,
   useToast,
   VStack,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { IoCreateOutline } from 'react-icons/io5';
+import { MdCancel } from 'react-icons/md';
 
 import Layout from '../../../../components/Layout';
 import { post } from '../../../../helpers/apiHelper';
 import webRoutes from '../../../../helpers/webRoutes';
 
 const CreateFloors = () => {
-  const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(1);
   const [name, setName] = useState('');
+  const { colorMode } = useColorMode();
   const router = useRouter();
   const toast = useToast();
 
+  const bg = useColorModeValue('#fafafa');
+
   return (
     <Layout title={['Create Floor']}>
-      <VStack as="form" spacing={5}>
-        <Heading fontSize="lg">Temporary Floor Creation Form</Heading>
+      <Flex as="form" borderRadius="md" direction="column" align="center" justify="center">
+        <VStack
+          px={[7, 14]}
+          py={[5, 10]}
+          spacing={5}
+          borderRadius="md"
+          w={['full', 'full', '60%']}
+          border={colorMode === 'light' ? '1px solid #000' : '1px solid #fff'}
+          bg={bg}
+        >
+          <Heading
+            textAlign="center"
+            fontSize={['md', 'md', 'lg']}
+            textTransform="uppercase"
+            color="#4e54c8"
+          >
+            Create a new floor!
+          </Heading>
 
-        <FormControl>
-          <Input
-            type="number"
-            id="number"
-            placeholder="1"
-            value={number}
-            isRequired
-            onChange={({ currentTarget: { value } }) => setNumber(value)}
-          />
-        </FormControl>
+          <Text textAlign="center" fontSize="sm">
+            Hello Owner! Please fill up some details first!
+          </Text>
 
-        <FormControl>
-          <Input
-            id="name"
-            placeholder="Name of the floor..."
-            value={name}
-            isRequired
-            onChange={({ currentTarget: { value } }) => setName(value)}
-          />
-        </FormControl>
+          <FormControl isRequired>
+            <FormLabel htmlFor="number">Floor Number</FormLabel>
+            <NumberInput
+              id="number"
+              defaultValue={1}
+              min={1}
+              max={20}
+              onChange={(value) => setNumber(value)}
+              focusBorderColor="green.500"
+              size="lg"
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
 
-        <ButtonGroup variant="outline" spacing={6}>
-          <Button
-            type="submit"
-            colorScheme="blue"
-            disabled={!name || !number}
-            onClick={async (e) => {
-              e.preventDefault();
+            <FormHelperText fontSize="xs">The floor number.</FormHelperText>
+          </FormControl>
 
-              const apiResponse = await post({ number, name, entity: 'floors' }, '/api/create');
+          <FormControl isRequired>
+            <FormLabel htmlFor="name">Floor Name</FormLabel>
+            <Input
+              id="name"
+              autoComplete="off"
+              placeholder="Name of the floor..."
+              value={name}
+              onChange={({ currentTarget: { value } }) => setName(value)}
+              focusBorderColor="green.500"
+              size="lg"
+            />
 
-              if (apiResponse.status === 'success') {
-                toast({
-                  title: 'Successfully created!',
-                  description: 'Thank you! You will be redirected shortly.',
-                  status: 'success',
+            <FormHelperText fontSize="xs">The floor name.</FormHelperText>
+          </FormControl>
+
+          <ButtonGroup variant="outline" spacing={6}>
+            <Button
+              type="submit"
+              leftIcon={<Icon as={IoCreateOutline} />}
+              colorScheme="teal"
+              disabled={!name || !number}
+              onClick={async (e) => {
+                e.preventDefault();
+
+                const apiResponse = await post({ number, name, entity: 'floors' }, '/api/create');
+
+                if (apiResponse.status === 'success') {
+                  toast({
+                    title: 'Successfully created!',
+                    description: 'Thank you! You will be redirected shortly.',
+                    status: 'success',
+                    isClosable: true,
+                  });
+
+                  return setTimeout(() => router.push(webRoutes.adminEntities('floors')), 1000);
+                }
+
+                return toast({
+                  title: 'Failed to create!',
+                  description: apiResponse.response.message,
+                  status: 'error',
                   isClosable: true,
                 });
-
-                return setTimeout(() => router.push(webRoutes.adminEntities('floors')), 1000);
-              }
-
-              return toast({
-                title: 'Failed to create!',
-                description: apiResponse.response.message,
-                status: 'error',
-                isClosable: true,
-              });
-            }}
-          >
-            Create
-          </Button>
-          <Button>Cancel</Button>
-        </ButtonGroup>
-      </VStack>
+              }}
+            >
+              Create
+            </Button>
+            <Button
+              leftIcon={<Icon as={MdCancel} />}
+              onClick={() => router.push(webRoutes.adminHomepage)}
+            >
+              Cancel
+            </Button>
+          </ButtonGroup>
+        </VStack>
+      </Flex>
     </Layout>
   );
 };
