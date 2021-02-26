6bf6346f4b4fdece223490bd7a9cdc466c3ac167
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-26 22:52:24 +0700

    fix: Fix typo on 'webRoutes'

--- web/helpers/webRoutes.js
+++ web/helpers/webRoutes.js
@@ -3,6 +3,7 @@ export default {
   adminHomepage: '/admin',
   adminEntities: (entityName) => `/admin/entities/${entityName}`,
   adminCreateEntities: (entityName) => `/admin/entities/create/${entityName}`,
+  adminEditEntities: (entityName, id) => `/admin/entities/edit/${entityName}?id=${id}`,
   adminOrders: `/admin/orders`,
   homepage: '/',
   listOfRooms: '/rooms',
