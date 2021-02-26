77364181be9954a93287ae3ad6bfa720cdb37b61
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-26 17:39:41 +0700

    feat: List all routes in 'webRoutes'

--- web/helpers/webRoutes.js
+++ web/helpers/webRoutes.js
@@ -2,4 +2,11 @@ export default {
   adminHomepage: '/admin',
   adminEntities: '/admin/entities',
   adminCreateEntities: (entityName) => `/admin/entities/create-${entityName}`,
+  adminOrders: `/admin/orders`,
+  homepage: '/',
+  listOfEntities: (entityName) => `/admin/entities/${entityName}`,
+  listOfRooms: '/rooms',
+  profile: '/profile',
+  roomDetail: (roomSlug) => `/rooms/${roomSlug}`,
+  signIn: '/sign-in',
 };
