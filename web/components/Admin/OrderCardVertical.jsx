98e3ad76f3e147bcbf1db7274ec6cf77f0c3ec99
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-26 21:02:01 +0700

    feat: Hydrate 'OrderCard' with data from backend

--- web/components/Admin/OrderCardVertical.jsx
+++ web/components/Admin/OrderCardVertical.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import {
   Badge,
   Button,
@@ -10,81 +11,202 @@ import {
   MenuList,
   Stack,
   Text,
+  useToast,
   VStack,
-  Wrap,
   WrapItem,
 } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
 
-const OrderCardVertical = () => {
+import { post } from '../../helpers/apiHelper';
+import webRoutes from '../../helpers/webRoutes';
+
+const OrderCardVertical = ({ data }) => {
+  const toast = useToast();
+  const router = useRouter();
+
+  const changeOrderStatus = async (status, orderId) => {
+    const apiResponse = await post({ status, orderId }, '/api/changeOrder');
+
+    return apiResponse;
+  };
+
   return (
-    <Wrap p={3} spacing={7} justify="center">
-      <WrapItem>
-        <Stack
-          direction="column"
-          bg="white"
-          p={4}
-          spacing={5}
-          justify="center"
-          borderWidth={1}
-          borderColor="white"
-          borderRadius="md"
-          boxShadow="0 30px 60px rgba(0, 0, 0, 0.15)"
-        >
-          <Badge colorScheme="messenger">ID: 5c8a1d5b0190b214360dc057</Badge>
+    <WrapItem>
+      <Stack
+        direction="column"
+        p={4}
+        spacing={5}
+        justify="center"
+        align="center"
+        borderWidth={1}
+        borderColor="white"
+        borderRadius="md"
+        boxShadow="0 30px 60px rgba(0, 0, 0, 0.15)"
+        textAlign="center"
+      >
+        <Badge colorScheme="messenger">{`ID: ${data._id}`}</Badge>
 
-          <VStack spacing={2}>
-            <Badge fontSize="sm" colorScheme="orange">
-              Order Details:
+        <VStack spacing={2}>
+          <Badge fontSize="sm" colorScheme="orange">
+            Order Details:
+          </Badge>
+          <Image
+            src={`${process.env.NEXT_PUBLIC_API_URL}/images/thumbnails/${data.room.thumbnail}`}
+            h="full"
+            borderRadius="full"
+          />
+          <Text>{data.room.name}</Text>
+          <Badge colorScheme="green">{data.status}</Badge>
+        </VStack>
+
+        <VStack>
+          <Heading fontSize="sm">Start/End Date</Heading>
+          <HStack spacing={3}>
+            <Badge fontSize="sm" colorScheme="red">
+              {new Date(data.startDate).toISOString().split('T')[0]}
             </Badge>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_API_URL}/images/thumbnails/ganyu.png`}
-              h="full"
-              borderRadius="full"
-            />
-            <Text>Ganyu</Text>
-            <Badge colorScheme="green">ORDERED</Badge>
-          </VStack>
-
-          <VStack>
-            <Heading fontSize="sm">Start/End Date</Heading>
-            <HStack spacing={3}>
-              <Badge fontSize="sm" colorScheme="red">
-                2021-02-22
-              </Badge>
-              <Badge fontSize="sm" colorScheme="linkedin">
-                2021-02-25
-              </Badge>
-            </HStack>
-          </VStack>
-
-          <VStack spacing={3}>
-            <Heading fontSize="sm">Buyer</Heading>
-            <Badge colorScheme="linkedin">Dzulfiqar Ramadhan</Badge>
-            <Heading fontSize="sm">Total Price</Heading>
-            <Badge colorScheme="orange">Rp. 5.000.000</Badge>
-            <Heading fontSize="sm">Associated Employee</Heading>
-            <Badge colorScheme="green">Nicholas Dwiarto Wirasbawa</Badge>
-          </VStack>
-
-          <HStack>
-            <Menu>
-              <MenuButton as={Button} size="xs" colorScheme="green">
-                Change Status
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Processed</MenuItem>
-                <MenuItem>Accepted</MenuItem>
-                <MenuItem>Finished</MenuItem>
-              </MenuList>
-            </Menu>
-            <Button size="xs" colorScheme="red">
-              Cancel Request
-            </Button>
+            <Badge fontSize="sm" colorScheme="linkedin">
+              {new Date(data.endDate).toISOString().split('T')[0]}
+            </Badge>
           </HStack>
-        </Stack>
-      </WrapItem>
-    </Wrap>
+        </VStack>
+
+        <VStack spacing={3}>
+          <Heading fontSize="sm">Buyer</Heading>
+          <Badge colorScheme="linkedin">{`${data.user.firstName} ${data.user.lastName}`}</Badge>
+          <Heading fontSize="sm">Total Price</Heading>
+          <Badge colorScheme="orange">{`Rp. ${data.totalPrice}`}</Badge>
+          <Heading fontSize="sm">Associated Employee</Heading>
+          <Badge colorScheme="green">{data.employee === null ? 'No Employee' : 'You'}</Badge>
+        </VStack>
+
+        <HStack>
+          <Menu>
+            <MenuButton as={Button} size="xs" colorScheme="green">
+              Change Status
+            </MenuButton>
+            <MenuList>
+              <MenuItem
+                onClick={async (e) => {
+                  e.preventDefault();
+
+                  const apiResponse = await changeOrderStatus('processed', data._id);
+
+                  if (apiResponse.status === 'success') {
+                    toast({
+                      title: 'Successfully updated!',
+                      description: 'This order is now yours to manage! Redirecting shortly...',
+                      status: 'success',
+                      isClosable: true,
+                    });
+
+                    return setTimeout(() => router.push(webRoutes.adminOrders), 5000);
+                  }
+
+                  return toast({
+                    title: 'Failed to process!',
+                    description: apiResponse.response.message,
+                    status: 'error',
+                    isClosable: true,
+                  });
+                }}
+              >
+                Processed
+              </MenuItem>
+              <MenuItem
+                onClick={async (e) => {
+                  e.preventDefault();
+
+                  const apiResponse = await changeOrderStatus('accepted', data._id);
+
+                  if (apiResponse.status === 'success') {
+                    toast({
+                      title: 'Successfully updated!',
+                      description: 'This order is now accepted! Redirecting shortly...',
+                      status: 'success',
+                      isClosable: true,
+                    });
+
+                    return setTimeout(() => router.push(webRoutes.adminOrders), 5000);
+                  }
+
+                  return toast({
+                    title: 'Failed to process!',
+                    description: apiResponse.response.message,
+                    status: 'error',
+                    isClosable: true,
+                  });
+                }}
+              >
+                Accepted
+              </MenuItem>
+              <MenuItem
+                onClick={async (e) => {
+                  e.preventDefault();
+
+                  const apiResponse = await changeOrderStatus('finished', data._id);
+
+                  if (apiResponse.status === 'success') {
+                    toast({
+                      title: 'Successfully updated!',
+                      description: 'This order is now finished! Redirecting shortly...',
+                      status: 'success',
+                      isClosable: true,
+                    });
+
+                    return setTimeout(() => router.push(webRoutes.adminOrders), 5000);
+                  }
+
+                  return toast({
+                    title: 'Failed to process!',
+                    description: apiResponse.response.message,
+                    status: 'error',
+                    isClosable: true,
+                  });
+                }}
+              >
+                Finished
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          <Button
+            size="xs"
+            colorScheme="red"
+            onClick={async (e) => {
+              e.preventDefault();
+
+              const apiResponse = await changeOrderStatus('cancelled', data._id);
+
+              if (apiResponse.status === 'success') {
+                toast({
+                  title: 'Successfully updated!',
+                  description: 'This order is now cancelled! Redirecting shortly...',
+                  status: 'success',
+                  isClosable: true,
+                });
+
+                return setTimeout(() => router.push(webRoutes.adminOrders), 5000);
+              }
+
+              return toast({
+                title: 'Failed to process!',
+                description: apiResponse.response.message,
+                status: 'error',
+                isClosable: true,
+              });
+            }}
+          >
+            Cancel Request
+          </Button>
+        </HStack>
+      </Stack>
+    </WrapItem>
   );
+};
+
+OrderCardVertical.propTypes = {
+  data: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default OrderCardVertical;
--- web/pages/admin/orders/index.jsx
+++ web/pages/admin/orders/index.jsx
@@ -1,20 +1,50 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Wrap } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
 
-import OrderCardHorizontal from '../../../components/Admin/OrderCardHorizontal';
-import OrderCardHorizontalTwo from '../../../components/Admin/OrderCardHorizontalTwo';
 import OrderCardVertical from '../../../components/Admin/OrderCardVertical';
 import Layout from '../../../components/Layout';
+import { getAuth } from '../../../helpers/apiHelper';
 
-const Admin = () => {
+export const getServerSideProps = async (ctx) => {
+  const token = ctx.req.cookies.jwt;
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanent: false,
+      },
+    };
+  }
+
+  const { data } = await getAuth(
+    `${process.env.PRIVATE_API_URL}/api/v1/orders/my-managed-orders`,
+    token
+  );
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const Admin = ({ data }) => {
   return (
     <Layout title={['Admin']}>
-      <Box h="full" w="full" bg="#fafafa">
-        <OrderCardVertical />
-        <OrderCardHorizontal />
-        <OrderCardHorizontalTwo />
+      <Box h="full" w="full">
+        <Wrap p={3} spacing={7} justify="center">
+          {data.map((el) => (
+            <OrderCardVertical data={el} />
+          ))}
+        </Wrap>
       </Box>
     </Layout>
   );
+};
+
+Admin.propTypes = {
+  data: PropTypes.arrayOf(Object).isRequired,
 };
 
 export default Admin;
