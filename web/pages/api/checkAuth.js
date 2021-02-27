008e8e7e09f4dbcbd5fb5aaa98c27ca72d80ee42
lauslim12 <nicholasdwiarto@yahoo.com>
2021-02-27 18:31:33 +0700

    feat: Add message and method not implemented for the proxy API

--- web/pages/api/checkAuth.js
+++ web/pages/api/checkAuth.js
@@ -4,15 +4,24 @@ export default async (req, res) => {
   const cookies = new Cookies(req, res);
   const token = cookies.get('jwt');
 
+  if (req.method !== 'POST') {
+    return res.status(501).json({
+      status: 'fail',
+      message: 'Method not implemented!',
+    });
+  }
+
   if (!req.body.key || !token) {
     return res.status(401).json({
       status: 'fail',
+      message: 'Not authorized!',
       authorized: false,
     });
   }
 
   return res.status(200).json({
     status: 'success',
+    message: 'You are authorized!',
     authorized: true,
     token,
   });
