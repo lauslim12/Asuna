b8bc9d3c0978db581ce5bc0aa0db6f056c4a9bc7
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-03 15:52:31 +0700

    feat: Implement baseline page for 'Visitors'

---
+++ web/pages/admin/visitors/[room].jsx
@@ -0,0 +1,11 @@
+import Layout from '../../../components/Layout';
+
+const VisitorsCreate = () => {
+  return (
+    <Layout title={['Create Visitor']}>
+      <p>Hello World!</p>
+    </Layout>
+  );
+};
+
+export default VisitorsCreate;
--- web/pages/admin/visitors/index.jsx
+++ web/pages/admin/visitors/index.jsx
@@ -1,10 +1,35 @@
+import PropTypes from 'prop-types';
+
 import Layout from '../../../components/Layout';
+import ListOfRooms from '../../../components/ListOfRooms/ListView';
+import { get } from '../../../helpers/apiHelper';
 import isAdministrator from '../../../utils/isAdministrator';
 
-export const getServerSideProps = async (ctx) => isAdministrator(ctx);
+export const getServerSideProps = isAdministrator(async () => {
+  const roomRequest = await get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/rooms`);
+  const floorRequest = await get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/floors`);
 
-const Visitors = () => {
-  return <Layout title={['Visitors']} />;
+  const maxFloor = Math.max(...floorRequest.data.map((e) => e.number), 1);
+
+  return {
+    props: {
+      rooms: roomRequest.data,
+      maxFloor,
+    },
+  };
+});
+
+const Visitors = ({ rooms, maxFloor }) => {
+  return (
+    <Layout title={['Visitors']}>
+      <ListOfRooms rooms={rooms} maxFloor={maxFloor} route="visitors" />
+    </Layout>
+  );
+};
+
+Visitors.propTypes = {
+  rooms: PropTypes.instanceOf(Object).isRequired,
+  maxFloor: PropTypes.number.isRequired,
 };
 
 export default Visitors;
