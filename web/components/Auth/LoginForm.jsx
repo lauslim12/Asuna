b474b5d13f2af96ebd6a89cf1784784ae7f6738e
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-03 15:08:16 +0700

    refactor: Refactor form with the new toast

--- web/components/Auth/LoginForm.jsx
+++ web/components/Auth/LoginForm.jsx
@@ -1,23 +1,21 @@
 import {
   Button,
   ButtonGroup,
-  Flex,
   FormControl,
   FormHelperText,
   FormLabel,
-  Heading,
   Icon,
   Input,
   Text,
-  useColorModeValue,
   useToast,
-  VStack,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { RiLoginCircleFill, RiRewindFill } from 'react-icons/ri';
 
 import { post } from '../../helpers/apiHelper';
+import FormHeading from '../Admin/Forms/FormHeading';
+import FormOverlay from '../Admin/Forms/FormOverlay';
 import { FailedOperationToast, SuccessfulOperationToast } from '../Toasts';
 
 const LoginForm = () => {
@@ -26,92 +24,75 @@ const LoginForm = () => {
   const toast = useToast();
   const router = useRouter();
 
-  const bg = useColorModeValue('#fafafa');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const apiResponse = await post({ username, password }, '/api/login');
+
+    if (apiResponse.status === 'success') {
+      SuccessfulOperationToast(toast, 'Welcome! Please wait for a second...');
+      return setTimeout(() => router.push('/'), 1000);
+    }
+
+    return FailedOperationToast(toast, apiResponse.response);
+  };
 
   return (
-    <Flex as="form" borderRadius="md" direction="column" align="center" justify="center">
-      <VStack
-        px={[7, 14]}
-        py={[5, 10]}
-        spacing={5}
-        borderRadius="md"
-        w={['full', 'full', '60%']}
-        bg={bg}
-      >
-        <Heading
+    <FormOverlay submitAction={handleSubmit}>
+      <FormHeading formTitle="Log in to your account!" />
+
+      <Text textAlign="center" fontSize="sm">
+        Hello! For us to provide better services, please login first!
+      </Text>
+
+      <FormControl isRequired>
+        <FormLabel htmlFor="username">Username</FormLabel>
+        <Input
           textAlign="center"
-          fontSize={['md', 'md', 'lg']}
-          textTransform="uppercase"
-          color="#4e54c8"
+          bg="white"
+          id="username"
+          placeholder="PatrickStar1350..."
+          value={username}
+          onChange={({ currentTarget: { value } }) => setUsername(value)}
+          focusBorderColor="green.500"
+          size="lg"
+        />
+        <FormHelperText fontSize="xs">Your username that you used to register.</FormHelperText>
+      </FormControl>
+
+      <FormControl isRequired>
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input
+          textAlign="center"
+          bg="white"
+          id="password"
+          type="password"
+          placeholder="••••••••"
+          value={password}
+          errorBorderColor="red.500"
+          minLength={8}
+          isInvalid={password.length < 8 && password.length > 0}
+          onChange={({ currentTarget: { value } }) => setPassword(value)}
+          focusBorderColor="green.500"
+          size="lg"
+        />
+        <FormHelperText fontSize="xs">Your password that you used to register.</FormHelperText>
+      </FormControl>
+
+      <ButtonGroup variant="outline" spacing={6}>
+        <Button
+          leftIcon={<Icon as={RiLoginCircleFill} />}
+          type="submit"
+          colorScheme="teal"
+          disabled={!username || !password || password.length < 8}
         >
-          Log in to your account!
-        </Heading>
-
-        <Text textAlign="center" fontSize="sm">
-          Hello! For us to provide better services, please login first!
-        </Text>
-
-        <FormControl isRequired>
-          <FormLabel htmlFor="username">Username</FormLabel>
-          <Input
-            textAlign="center"
-            bg="white"
-            id="username"
-            placeholder="PatrickStar1350..."
-            value={username}
-            onChange={({ currentTarget: { value } }) => setUsername(value)}
-            focusBorderColor="green.500"
-            size="lg"
-          />
-          <FormHelperText fontSize="xs">Your username that you used to register.</FormHelperText>
-        </FormControl>
-
-        <FormControl isRequired>
-          <FormLabel htmlFor="password">Password</FormLabel>
-          <Input
-            textAlign="center"
-            bg="white"
-            id="password"
-            type="password"
-            placeholder="••••••••"
-            value={password}
-            errorBorderColor="red.500"
-            minLength={8}
-            isInvalid={password.length < 8 && password.length > 0}
-            onChange={({ currentTarget: { value } }) => setPassword(value)}
-            focusBorderColor="green.500"
-            size="lg"
-          />
-          <FormHelperText fontSize="xs">Your password that you used to register.</FormHelperText>
-        </FormControl>
-
-        <ButtonGroup variant="outline" spacing={6}>
-          <Button
-            leftIcon={<Icon as={RiLoginCircleFill} />}
-            type="submit"
-            colorScheme="teal"
-            disabled={!username || !password || password.length < 8}
-            onClick={async (e) => {
-              e.preventDefault();
-
-              const apiResponse = await post({ username, password }, '/api/login');
-
-              if (apiResponse.status === 'success') {
-                SuccessfulOperationToast(toast, 'Welcome! Please wait for a second...');
-                return setTimeout(() => router.push('/'), 1000);
-              }
-
-              return FailedOperationToast(toast, apiResponse.response);
-            }}
-          >
-            Sign In
-          </Button>
-          <Button leftIcon={<Icon as={RiRewindFill} />} colorScheme="blue">
-            Cancel
-          </Button>
-        </ButtonGroup>
-      </VStack>
-    </Flex>
+          Sign In
+        </Button>
+        <Button leftIcon={<Icon as={RiRewindFill} />} colorScheme="blue">
+          Cancel
+        </Button>
+      </ButtonGroup>
+    </FormOverlay>
   );
 };
 
