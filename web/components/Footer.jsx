3a377ab152f4c131423c8e9949820e39e2a34bed
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-04 15:21:30 +0700

    feat: Add footer

--- web/components/Footer.jsx
+++ web/components/Footer.jsx
@@ -1,11 +1,25 @@
-import { HStack } from '@chakra-ui/react';
+import { chakra, HStack, Icon, Link, Tooltip, VStack } from '@chakra-ui/react';
+import { FaGithub, FaHeart } from 'react-icons/fa';
 
-const Footer = () => {
-  return (
-    <HStack p={3} spacing={2} bg="gray.700" color="white">
-      <p>This is a footer.</p>
+const Footer = () => (
+  <VStack as="footer" align="stretch" p={3} spacing={1} color="gray.500">
+    <chakra.div fontSize="xs" textAlign="center">
+      <p>Asuna is an open-source, full-stack web application to manage a scalable building.</p>
+    </chakra.div>
+
+    <HStack spacing={4} justify="center">
+      <Tooltip label="GitHub">
+        <Link href="https://github.com/lauslim12/Asuna" isExternal>
+          <Icon as={FaGithub} />
+        </Link>
+      </Tooltip>
+      <Tooltip label="nicholasdw.com">
+        <Link href="https://nicholasdw.com" isExternal>
+          <Icon as={FaHeart} />
+        </Link>
+      </Tooltip>
     </HStack>
-  );
-};
+  </VStack>
+);
 
 export default Footer;
