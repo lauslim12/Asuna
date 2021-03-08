26ec9ecbf83db20af1ed6053a30dc149e9704980
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-08 16:10:15 +0700

    refactor: Refactor login for readability

--- web/pages/api/login.js
+++ web/pages/api/login.js
@@ -2,29 +2,33 @@ import axios from 'axios';
 import Cookies from 'cookies';
 
 export default async (req, res) => {
-  const { username, password } = req.body;
-  let response;
-
-  try {
-    response = await axios.post(`${process.env.PRIVATE_API_URL}/api/v1/users/login`, {
-      username,
-      password,
-    });
-  } catch (e) {
-    return res.status(401).json({
-      status: 'fail',
-      response: e.response.data,
-    });
-  }
-
   const cookies = new Cookies(req, res);
-  cookies.set('jwt', response.data.token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000),
-    sameSite: 'lax',
-  });
+  const dataToBeSent = {
+    username: req.body.username,
+    password: req.body.password,
+  };
 
-  return res.status(200).json({
-    status: 'success',
-  });
+  return axios
+    .post(`${process.env.PRIVATE_API_URL}/api/v1/users/login`, dataToBeSent)
+    .then((response) => {
+      if (response.data.status === 'success') {
+        cookies.set('jwt', response.data.token, {
+          httpOnly: true,
+          expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000),
+          sameSite: 'lax',
+        });
+
+        return res.status(200).json({
+          status: 'success',
+        });
+      }
+
+      return null;
+    })
+    .catch((err) => {
+      return res.status(401).json({
+        status: 'fail',
+        message: err.response.data.message,
+      });
+    });
 };
