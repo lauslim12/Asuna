b494cc796511f33956a8ef4ec033cdfba27b5ebd
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-09 14:48:26 +0700

    feat: Add custom spinner

---
+++ web/components/CustomSpinner.jsx
@@ -0,0 +1,10 @@
+import { Spinner, Text, VStack } from '@chakra-ui/react';
+
+const CustomSpinner = () => (
+  <VStack spacing={5}>
+    <Text>Please wait while we are processing your request!</Text>
+    <Spinner size="xl" color="green.300" thickness="4px" emptyColor="gray.300" />
+  </VStack>
+);
+
+export default CustomSpinner;
