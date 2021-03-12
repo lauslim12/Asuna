9bea5e05885e01d0cfe36091b0f5f4cb9e3e5d96
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-13 00:27:10 +0700

    feat: Implement custom font: 'Lato'

--- web/components/Layout.jsx
+++ web/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { chakra, Flex, Spacer } from '@chakra-ui/react';
 import Head from 'next/head';
+import { GoogleFonts } from 'next-google-fonts';
 import PropTypes from 'prop-types';
 
 import Footer from './Footer';
@@ -8,16 +9,10 @@ import Header from './Header';
 const Layout = ({ children, title }) => {
   return (
     <>
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" />
       <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <link rel="icon" href="favicon.png" type="image/png" />
-        <title>
-          {[...title.map((x) => x?.trim()).filter((x) => x), 'Project Asuna'].join(' · ')}
-        </title>
+        <title>{[...title.map((x) => x?.trim()).filter((x) => x), 'Asuna'].join(' · ')}</title>
       </Head>
-
       <Flex minH="100vh" direction="column" maxW="1200px" mx="auto">
         <Header />
 
--- web/pages/_app.jsx
+++ web/pages/_app.jsx
@@ -1,21 +1,42 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
+import 'focus-visible/dist/focus-visible';
 import '../styles/NProgress.scss';
 import '../styles/Selection.scss';
 import '../styles/DatePicker.scss';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import Head from 'next/head';
 
 import NProgress from '../components/NProgress';
 import UserContextProvider from '../components/UserProvider';
 import scrollPreserver from '../utils/scrollPreserver';
+
+const fallbackFonts =
+  '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
 
 const App = ({ Component, pageProps }) => {
   scrollPreserver();
 
   return (
     <UserContextProvider>
-      <ChakraProvider>
+      <ChakraProvider
+        resetCSS
+        theme={extendTheme({
+          fonts: {
+            body: `Lato, ${fallbackFonts}`,
+            heading: `Lato, ${fallbackFonts}`,
+          },
+        })}
+      >
+        <Head>
+          <meta charSet="UTF-8" />
+
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+          <link rel="icon" href="/favicon.png" type="image/png" />
+        </Head>
+
         <NProgress />
         <Component {...pageProps} />
       </ChakraProvider>
