46fff4253f2a264981c27e86cacb08447a799356
lauslim12 <nicholasdwiarto@yahoo.com>
2021-04-08 09:27:31 +0700

    feat: Change data to 'isValid'

--- api/models/voucherModel.js
+++ api/models/voucherModel.js
@@ -10,25 +10,9 @@ const voucherSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A voucher must have a discount!'],
   },
-  startDate: {
-    type: Date,
-    required: [true, 'A voucher must have a start date!'],
-    validate: {
-      validator: function (value) {
-        return new Date(Date.now()) < new Date(value);
-      },
-      message: 'The start date must be equal or after this day!',
-    },
-  },
-  endDate: {
-    type: Date,
-    required: [true, 'A voucher must have an end date!'],
-    validate: {
-      validator: function (value) {
-        return new Date(value) > new Date(this.startDate);
-      },
-      message: 'The end date must be after the start date!',
-    },
+  isValid: {
+    type: Boolean,
+    default: true,
   },
 });
 
