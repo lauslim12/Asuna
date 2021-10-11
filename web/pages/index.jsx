56664e01d40788fe2f274b99a6391bb2bbbc926c
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-10-11 11:07:38 +0700

    feat: Remove authors, repo is enough

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -1,13 +1,9 @@
-import dynamic from 'next/dynamic';
-
 import About from '../components/Home/About';
 import Go from '../components/Home/Go';
 import Hero from '../components/Home/Hero';
 import Padding from '../components/Home/Padding';
 import Services from '../components/Home/Services';
 import Layout from '../components/Layout';
-
-const CreatorsSection = dynamic(() => import('../components/Home/CreatorsSection'));
 
 const Home = () => {
   return (
@@ -34,7 +30,6 @@ const Home = () => {
         bgLight="red.500"
       />
       <Go />
-      <CreatorsSection />
     </Layout>
   );
 };
